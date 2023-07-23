42b0ac3008345c2e21c9affb2001324e8a43e09c
dyprodg <dennisdiepolder@outlook.com>
2023-07-23 03:22:50 +0200

    seven

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = ({ heading, message }) => {
     backgroundSize: 'cover',
     backgroundPosition: 'center',  
     width: '100%',
-    height: '100%',
+    height: '100vh',
     position: 'absolute',
     zIndex: '1',
   };
@@ -18,19 +18,21 @@ const Hero = ({ heading, message }) => {
     <div className='relative overflow-hidden h-screen'>
       <div style={backgroundStyle}></div>
       {/* Overlay */}
-      <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/70 z-[2]' />
-      <div className='p-5 text-white z-[2] mt-[-10rem] relative'>
-        <h2 className='text-5xl font-bold'>{heading}</h2>
-        <p className='py-5 text-xl'>{message}</p>
-        <div className='flex justify-center'>
+      <div className='absolute top-0 left-0 right-0 bottom-0 bg-black bg-opacity-70 z-10' />
+      <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-20 text-white'>
+        <div className='text-center'>
+          <h2 className='text-5xl font-bold'>{heading}</h2>
+          <p className='py-5 text-xl'>{message}</p>
+        </div>
+        <div className='flex justify-center mt-10'>
           <FaInstagram
             onClick={() => window.open('https://www.instagram.com/bearson.tattoo/', '_blank')}
-            className='cursor-pointer hover:scale-110 ease-in duration-200 mr-8'
+            className='cursor-pointer hover:scale-110 transition-all duration-200 mx-8'
             size={100}
           />
           <FaWhatsapp
             onClick={() => window.open('https://wa.me/+4916091208750', '_blank')}
-            className='cursor-pointer hover:scale-110 ease-in duration-200 mr-8'
+            className='cursor-pointer hover:scale-110 transition-all duration-200 mx-8'
             size={100}
           />
         </div>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -27,7 +27,7 @@ const Navbar = () => {
   return (
     <div
       style={{ backgroundColor: `${color}` }}
-      className='fixed left-0 top-0 w-full z-10 ease-in duration-300'
+      className='fixed left-0 top-0 w-full z-30 ease-in duration-300'
     >
       <div className='max-w-[1240px] m-auto flex justify-between items-center p-4 text-white'>
         <Link href='/'>
