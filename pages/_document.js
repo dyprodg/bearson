16363206a855b9878b5e8ec413e937eddb8bf2bf
dyprodg <dennisdiepolder@outlook.com>
2023-07-26 16:55:13 +0200

    font4

--- pages/_document.js
+++ pages/_document.js
@@ -11,9 +11,6 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="shortcut icon" href="/bearson.ico" />
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-          <link href="https://fonts.googleapis.com/css2?family=Fresca&display=swap" rel="stylesheet" />
         </Head>
         <body>
           <Main />
